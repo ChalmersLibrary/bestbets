a539a89fd734db92838e360841a09d7f83b3beb8
Lars Andersson <lars.j.andersson@chalmers.se>
2018-07-26 15:21:23 +0200

    Use correct filename.

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var auth = require('basic-auth');
 const path = require('path');
 
+const configFilename = 'recom_en.json';
+
 router.get('/config', function(req, res, next) {
   let credentials = auth(req);
 
@@ -16,7 +18,7 @@ router.get('/config', function(req, res, next) {
     res.setHeader('WWW-Authenticate', 'Basic realm="Access to Best Bets configuration"');
     res.end('Access denied');
   } else {
-    fs.readFile(path.join(baseDir, 'public', 'config.json'), function (err, data) {
+    fs.readFile(path.join(baseDir, 'public', configFilename), function (err, data) {
       if (err) {
         res.status(500).send('Failed to load configuration.');
       } else {
@@ -39,7 +41,7 @@ router.post('/config', function(req, res, next) {
     res.setHeader('WWW-Authenticate', 'Basic realm="Access to Best Bets configuration"');
     res.end('Access denied');
   } else {
-    fs.writeFile(path.join(baseDir, 'public', 'config.json'), JSON.stringify(req.body, null, 2), function(err) {
+    fs.writeFile(path.join(baseDir, 'public', configFilename), JSON.stringify(req.body, null, 2), function(err) {
       if (err) {
         console.error('Error: ' + err);
         res.status(500).send('Failed to save configuration.');
